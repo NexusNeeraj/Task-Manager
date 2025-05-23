37cbac0fd56a968eeba29114ff7e9eace37ccbff
Neeraj Gupta <na1993194@gmail.com>
2025-05-23 15:27:15 +0530

    feat:db-connection-string

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
+
 const app = express();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -9,4 +13,3 @@ app.get("/", (req, res) => res.send("API is running"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
