7aa1604314dbd0ced76fa46641bfccefb0b6515c
Neeraj Gupta <na1993194@gmail.com>
2025-05-23 15:46:16 +0530

    Add:Task-schema

---
+++ server/models/Task.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  status: {
+    type: String,
+    enum: ["todo", "in progress", "done"],
+    default: "todo",
+  },
+  priority: {
+    type: String,
+    enum: ["low", "medium", "high"],
+    default: "medium",
+  },
+  dueDate: Date,
+  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  documents: [String], // file paths
+}, { timestamps: true });
+
+module.exports = mongoose.model("Task", taskSchema);
